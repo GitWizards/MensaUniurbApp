460426fe0bef9479076e30466e940dadad820c34
Dawid Weglarz <dawid.weglarz95@gmail.com>
2023-05-05 10:06:14 +0200

    Removed allergens and update SDK

--- lib/widgets/mealTile.dart
+++ lib/widgets/mealTile.dart
@@ -1,54 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:mensa_uniurb/utils/humanNames.dart';
 
 Widget getMealTile(
     BuildContext context, String title, List children, Icon icon) {
   return ExpansionTile(
       initiallyExpanded: true,
-      iconColor: Theme.of(context).colorScheme.primary,
-      textColor: Theme.of(context).colorScheme.primary,
+      iconColor: Theme.of(context).colorScheme.secondary,
+      textColor: Theme.of(context).colorScheme.secondary,
       leading: icon,
       title: Text(
         title,
         style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
       ),
       children: children.map((child) {
-        return ListTile(
-          title: Text("• " + removeIngredients(child)),
-          trailing: parseIngredients(child).isNotEmpty
-              ? IconButton(
-                  icon: Icon(
-                    FontAwesomeIcons.circleInfo,
-                    size: 15,
-                    color: Colors.grey,
-                  ),
-                  onPressed: () {
-                    showDialog<void>(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return AlertDialog(
-                          title: const Text('Lista allergeni'),
-                          content: SingleChildScrollView(
-                            child: ListBody(
-                              children: parseIngredients(child),
-                            ),
-                          ),
-                          actions: <Widget>[
-                            TextButton(
-                              child: const Text('OK'),
-                              onPressed: () {
-                                Navigator.of(context).pop();
-                              },
-                            ),
-                          ],
-                        );
-                      },
-                    );
-                  },
-                )
-              : null,
-        );
+        return ListTile(title: Text("• " + child));
       }).toList());
 }
 
